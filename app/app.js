c58cb6c2cfa7a67a5f9d4662aca8839b1823f967
shannonphu <shannonphu@sbcglobal.net>
2017-10-31 17:29:45 -0700

    integrated socket.io

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const http = require('http');
 const app = express();
+const server = http.createServer(app);
+const socket = require('./server-socket-event-handler')(server);
 
 app.set('view engine', 'hbs');
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
 	res.render('home', {
@@ -10,6 +14,6 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
 	console.log("Listening on port 3000");
 });
---
+++ server-socket-event-handler.js
@@ -0,0 +1,16 @@
+// Handles socket connections and event callbacks
+module.exports = function(server) {
+    const io = require('socket.io').listen(server);
+
+    io.sockets.on('connection', function (socket) {
+    	console.log("A connection was made");
+
+        socket.on('disconnect', function(){
+          console.log('A user disconnected');
+        });
+
+        socket.on('chat message', function(msg){
+          console.log('message: ' + msg);
+        });
+    });
+};
