1d91d69cc3e66761a505cf703564f9b8ccd002d3
shannonphu <shannonphu@sbcglobal.net>
2017-11-01 02:21:18 -0700

    integrated mongodb in Docker

--- app/app.js
+++ app/app.js
@@ -1,14 +1,7 @@
 const express = require('express');
-const http = require('http');
 const app = express();
-const server = http.createServer(app);
-const socket = require('./server-socket-event-handler')(server);
-
-// Database Models
-const Comment = require('./db/comment.js');
 
 app.set('view engine', 'hbs');
-app.use(express.static('public'))
 
 app.get('/', (req, res) => {
 	res.render('home', {
@@ -17,6 +10,6 @@ app.get('/', (req, res) => {
 	});
 });
 
-server.listen(3000, () => {
+app.listen(3000, () => {
 	console.log("Listening on port 3000");
 });
