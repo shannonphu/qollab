7bdd221600d3b3af8495dbe9791d65a9ee5904d5
shannonphu <shannonphu@sbcglobal.net>
2017-11-02 17:40:41 -0700

    Improved whiteboard syncing to resync when user disconnects and rejoins

--- app/public/js/client-socket-event-handler.js
+++ app/public/js/client-socket-event-handler.js
@@ -4,6 +4,6 @@ socket.on('connect', function() {
 	console.log("A new client connected to the websocket");
 });
 
-socket.on('canvas:update', function(pathJSON) {
-	canvas.loadFromJSON(pathJSON);
+socket.on('canvas:update', function(canvasJSON) {
+	canvas.loadFromJSON(canvasJSON);
 });
