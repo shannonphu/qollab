39d66e2482a378a7d1ce4470b22d25192c5f7853
Shannon Phu <shannonphu@sbcglobal.net>
2017-11-05 16:23:58 -0800

    Improved whiteboard syncing to resync when user disconnects and rejoins (#9)

--- app/server-socket-event-handler.js
+++ app/server-socket-event-handler.js
@@ -2,15 +2,30 @@
 module.exports = function(server) {
     const io = require('socket.io').listen(server);
 
+    // TODO: map lecture to canvas
+    let canvasHistory = [];
+
     io.sockets.on('connection', function (socket) {
     	console.log("A connection was made");
+
+        // Fill canvas if new connection made midway through session
+        if (canvasHistory.length > 0) {
+            socket.emit('canvas:update', JSON.stringify(getCanvasJSON(canvasHistory)));
+        }
 
         socket.on('disconnect', function(){
           console.log('A user disconnected');
         });
 
+        // Add path to history and update all clients
         socket.on('path:drawn', function(pathJSON) {
-            socket.broadcast.emit('canvas:update', pathJSON);
+            let path = JSON.parse(pathJSON);
+            canvasHistory.push(path);
+            socket.broadcast.emit('canvas:update', JSON.stringify(getCanvasJSON(canvasHistory)));
         });
     });
+
+    var getCanvasJSON = function(pathHistory) {
+        return {"objects": pathHistory};
+    }
 };
