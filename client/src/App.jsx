d6167ee82238a5fd85fdfca58442ff67436d5fd4
shannonphu <shannonphu@sbcglobal.net>
2017-11-09 18:45:54 -0800

    made component for Nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import { Route, Redirect, Switch, Link } from 'react-router-dom'
 
+import Nav from './components/Nav';
+
 class App extends Component {
   constructor (props) {
     super(props)
@@ -9,7 +11,8 @@ class App extends Component {
 
   render () {
     return (
-      <div className='App container'>
+      <div className='App'>
+          <Nav title="Lecture Title Placeholder" />
           <p>Hello, world</p>
       </div>
     )
