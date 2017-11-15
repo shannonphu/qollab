9e2767b1e5b6fbf1cf041aacbd0aa0801e18d92a
shannonphu <shannonphu@sbcglobal.net>
2017-11-14 19:47:51 -0800

    renamed allReducers to index

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import allReducers from './reducers/allReducers.js';
+import reducers from './reducers/index.js';
 import App from './App.jsx';
 
-const store = createStore(allReducers);
+const store = createStore(reducers);
 
 const router = (
 	<Provider store={store}>
