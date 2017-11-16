267663baa45ecc0dd498160f271e961e065ecfd0
shannonphu <shannonphu@sbcglobal.net>
2017-11-16 01:12:48 -0800

    set hardcoded styling for pencil tool

--- client/src/components/react-sketch/pencil.js
+++ client/src/components/react-sketch/pencil.js
@@ -1,11 +1,10 @@
 import FabricCanvasTool from './fabrictool'
 
 class Pencil extends FabricCanvasTool {
-
     configureCanvas(props) {
         this._canvas.isDrawingMode = true;
-        this._canvas.freeDrawingBrush.width = props.lineWidth;
-        this._canvas.freeDrawingBrush.color = props.lineColor;
+        this._canvas.freeDrawingBrush.width = 5;
+        this._canvas.freeDrawingBrush.color = props.pencilLineColor;
     }
 }
 
