acb7e755c651c6240a510f2e574a2ee0964654ee
shannonphu <shannonphu@sbcglobal.net>
2017-11-17 00:46:41 -0800

    integrated redux-socketio and made actions for syncing whiteboard fully

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import reducers from './reducers/index.js';
 import App from './App.jsx';
 
-const store = createStore(reducers);
+import createSocketIoMiddleware from 'redux-socket.io';
+import io from 'socket.io-client';
+const socket = io('http://localhost:3003/');
+const socketIoMiddleware = createSocketIoMiddleware(socket, "socket/");
+const store = applyMiddleware(socketIoMiddleware)(createStore)(reducers);
+store.dispatch({type:'socket/hello', data:'Hello!'});
 
 const router = (
 	<Provider store={store}>
--- client/src/reducers/comment.js
+++ client/src/reducers/comment.js
@@ -1,29 +1,32 @@
 var CommentsReducer = (state = {
 	addAnnotationActive: false,
 	activeAnnotationId: null,
-	comments: initialComments()
+	comments: initialComments(),
+	lectureCode: null,
+	canvasJSON: null
 }, action) => {
 	switch (action.type) {
 		case 'ADD_ANNOTATION':
 			return {
+				...state,
 				addAnnotationActive: true,
-				activeAnnotationId: null,
-				comments: [...state.comments]
+				activeAnnotationId: null
 			};
 		case 'STORE_ANNOTATION_ID':
 			return {
+				...state,
 				addAnnotationActive: action.annotationId ? true : false,
-				activeAnnotationId: action.annotationId,
-				comments: [...state.comments]
+				activeAnnotationId: action.annotationId
 			};
 		case 'REMOVE_ANNOTATION':
 			return {
+				...state,
 				addAnnotationActive: false,
-				activeAnnotationId: action.annotationId,
-				comments: [...state.comments]
+				activeAnnotationId: action.annotationId
 			};
 		case 'ADD_COMMENT':
 			return {
+				...state,
 				addAnnotationActive: false,
 				activeAnnotationId: null,
 				comments: [
@@ -31,6 +34,24 @@ var CommentsReducer = (state = {
 					Object.assign({}, action.comment)
 				]
 			};
+		case 'STORE_JOIN_CODE':
+			return {
+				...state,
+				lectureCode: action.joinCode
+			}
+		case 'LOAD_CANVAS_FROM_JSON':
+			// Only update the stored canvasJSON if the lecture code is for this lecture
+			let joinCode = action.canvasJSON["joinCode"];
+			if (state.lectureCode !== joinCode) {
+				return state;
+			}
+
+			let canvasJSON = action.canvasJSON["data"];			
+			return {
+				...state,
+				joinCode: joinCode,
+				canvasJSON: canvasJSON
+			}
 		default:
 			return state;
 	}
