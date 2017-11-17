dbdeafdec5c0c6c896e063457e4ac1feab247e3a
shannonphu <shannonphu@sbcglobal.net>
2017-11-17 12:52:42 -0800

    pass comment through socket connection

--- socket_api/server-socket-event-handler.js
+++ socket_api/server-socket-event-handler.js
@@ -12,17 +12,33 @@ module.exports = function (server, canvasHistory) {
         socket.on('action', (action) => {
             switch (action.type) {
                 case 'socket/CANVAS_UPDATED':
-                    let json = action.canvasJSON;
-                    let joinCode = json['joinCode'];
-                    let canvasData = json['data'];
-                    canvasHistory[joinCode] = canvasData;
-                    socket.broadcast.emit('action', {
-                        type: 'LOAD_CANVAS_FROM_JSON',
-                        canvasJSON: {
-                            "joinCode": joinCode,
-                            "data": canvasData
-                        }
-                    });
+                    {
+                        let json = action.canvasJSON;
+                        let joinCode = json['joinCode'];
+                        let canvasData = json['data'];
+                        canvasHistory[joinCode] = canvasData;
+                        socket.broadcast.emit('action', {
+                            type: 'LOAD_CANVAS_FROM_JSON',
+                            canvasJSON: {
+                                "joinCode": joinCode,
+                                "data": canvasData
+                            }
+                        });
+                    }
+                    break;
+                case 'socket/COMMENT_ADDED':
+                    {
+                        let json = action.data;
+                        let joinCode = json['joinCode'];
+                        let comment = json['comment'];
+                        console.log(json);
+                        console.log(comment);
+                        // canvasHistory[joinCode] = canvasData;
+                        socket.broadcast.emit('action', {
+                            type: 'SYNC_NEW_COMMENT',
+                            comment: comment
+                        });
+                    }
                     break;
                 default:
                     break;
