fa6bcc770ed80d75d227dde7e93fb3fd2f11c2af
shannonphu <shannonphu@sbcglobal.net>
2017-11-27 10:48:09 -0800

    added lecture refs to User object and methods to add Lecture and getLectures

--- db_api/db/seed.js
+++ db_api/db/seed.js
@@ -6,10 +6,14 @@ User.insert((instructor_1) => {
 	Lecture.insert("COM SCI 130", instructor_1.id, (newLecture) => {
 		console.log("Inserted new lecture with code " + newLecture.joinCode);
 		console.log('Instructor Id is ' + newLecture.instructor);
+
+		instructor_1.addLecture(newLecture);
 	});
 
 	Lecture.insert("COM SCI 181", instructor_1.id, (newLecture) => {
 		console.log("Inserted new lecture with code " + newLecture.joinCode);
 		console.log('Instructor Id is ' + newLecture.instructor);
+
+		instructor_1.addLecture(newLecture);
 	});
 });
--- db_api/db/user.js
+++ db_api/db/user.js
@@ -5,7 +5,9 @@
 module.exports = (function () {
     let mongoose = require('mongoose');
 
-    var userSchema = new mongoose.Schema();
+    var userSchema = new mongoose.Schema({
+        lectures: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lecture', unique: true }]
+    });
 
     /**
      * @summary inserts a new User object into our database
@@ -28,6 +30,58 @@ module.exports = (function () {
                 callback(user);
             }
         });
+    }
+
+    /**
+     * @summary inserts an ID of a lecture object into this user
+     * @param {Lecture} lecture object to insert into user
+     * @param {function} callback to execute after inserting lecture into user
+     * @returns {User} The atual user mongoDB object
+     * @memberof module:userDB
+     * @example
+     * instructor.addLecture(newLecture, (updatedInstructor) => {
+     * 
+     * });
+     */
+    userSchema.methods.addLecture = function (lecture, callback) {
+        User.findOneAndUpdate(
+            { _id : this._id },
+            { $push: { "lectures": lecture._id } },
+            { safe: true, upsert: true, new: true },
+            function (err, user) {
+                if (err) {
+                    throw err;
+                }
+
+                if (callback) {
+                    callback(user);
+                }
+            }
+        );
+    }
+
+    /**
+     * @summary retrieves all lecture objects belonging to this user
+     * @param {function} callback to execute after getting all lecture objects
+     * @returns {[Lecture]} array of lectures belonging to this user
+     * @memberof module:userDB
+     * @example
+     * instructor.getLectures((lectures) => {
+     * 
+     * });
+     */
+    userSchema.methods.getLectures = function (callback) {
+        User.findOne({ _id : this._id })
+            .populate('lectures')
+            .exec(function (err, lectures) {
+                if (err) {
+                    throw err;
+                }
+
+                if (callback) {
+                    callback(lectures);
+                }
+            });
     }
 
     let User = mongoose.model('User', userSchema);
