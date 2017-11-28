2b501f00411a1ab53fe27c844bf3de5e9db5814a
Tim Lam <katimlam@yahoo.com>
2017-11-27 16:12:51 -0800

    Change showCommentFormToogle tooltip style

--- client/src/components/toolSet/ShowResolvedCommentsToggle.jsx
+++ client/src/components/toolSet/ShowResolvedCommentsToggle.jsx
@@ -5,17 +5,24 @@ import ReactTooltip from 'react-tooltip'
 import * as commentsActions from '../../actions/comments';
 
 class ShowResolvedCommentsToggle extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.getTooltipContent = this.getTooltipContent.bind(this);
+    }
+
+    getTooltipContent() {
+        return (this.props.showResolvedCommentsToggled) ? "Hide Resolved Comments" : "Show Resolved Comments";
+    }
+
     render() {
         const icon = this.props.showResolvedCommentsToggled ? <i className="material-icons">visibility_off</i> : <i className="material-icons">visibility</i>;
-        const tooltip = this.props.showResolvedCommentsToggled ? <span>Hide Resolved Comments</span> : <span>Show Resolved Comments</span>
         return(
             <div className="ShowResolvedCommentsToggle">
                 <a data-tip data-for="ShowResolvedCommentsToggleTooltip" onClick={this.props.toggleShowResolvedComments} className="btn-floating yellow">
                     {icon}
                 </a>
-                <ReactTooltip place="left" type="info" id="ShowResolvedCommentsToggleTooltip">
-                    {tooltip}
-                </ReactTooltip>
+                <ReactTooltip place="left" type="info" effect="solid" id="ShowResolvedCommentsToggleTooltip" getContent={[this.getTooltipContent, 100]} />
             </div>
         )
     }
