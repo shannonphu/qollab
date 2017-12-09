9efddf6005bf602423c3c1dc3966576277de3241
shannonphu <shannonphu@sbcglobal.net>
2017-12-09 12:24:48 -0800

    Change nav bar icons and text

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -14,23 +14,23 @@ class Nav extends Component {
                 <nav className="light-blue lighten-1" role="navigation">
                     <div className="nav-wrapper">
                         <div className="brand-logo center">{this.props.lecture ? this.props.lecture.title : "Qollab"}</div>
+                        <ul id="nav-mobile" className="left hide-on-med-and-down">
+                            <li><a href="/"><i className="material-icons left">home</i></a></li>
+                        </ul>
                         <a href="#" data-activates="mobile-collapse" className="button-collapse">
                             <i className="material-icons">menu</i>
                         </a>
-                        <ul id="nav-mobile" className="left hide-on-med-and-down">
-                            <li><a href="/"><i className="material-icons left">home</i>Qollab</a></li>
-                        </ul>
                         <ul id="nav-mobile" className="right hide-on-med-and-down">
                             <li><a href="#"><i className="material-icons left">people</i>Students</a></li>
                             <li><a href="#"><i className="material-icons left">insert_chart</i>Poll</a></li>
-                            <li><a href='http://localhost:3005/auth/google'><i className="material-icons left">insert_chart</i>account_circle</a></li>
+                            <li><a href='http://localhost:3005/auth/google'><i className="material-icons left">account_circle</i>Login</a></li>
                         </ul>
                         {/* Mobile Collapse Buttons */}
                         <ul className="side-nav" id="mobile-collapse">
-                            <li><a href="/"><i className="material-icons left">home</i>Qollab</a></li>
+                            <li><a href="/"><i className="material-icons left">home</i>Home</a></li>
                             <li><a href="#"><i className="material-icons left">people</i>Students</a></li>
                             <li><a href="#"><i className="material-icons left">insert_chart</i>Poll</a></li>
-                            <li><a href='http://localhost:3005/auth/google'><i className="material-icons left">insert_chart</i>account_circle</a></li>
+                            <li><a href='http://localhost:3005/auth/google'><i className="material-icons left">account_circle</i>Login</a></li>
                         </ul>
                     </div>
                 </nav>
