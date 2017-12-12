9dd732238e82ec67a498c836ab287d3ee02011e5
Jerry Liu <jerrylzy@ucla.edu>
2017-12-12 11:35:14 -0800

    Fix E11000 duplicate key error collection 	* db_api/db/seed.js: Added missing ID field 	You may need to clean up docker images to successfully login. 	``` 	docker stop $(docker ps -a -q) 	docker rm $(docker ps -a -q) 	```

--- db_api/db/seed.js
+++ db_api/db/seed.js
@@ -1,8 +1,10 @@
 const Lecture = require('./lecture.js');
 const User = require('./user.js');
 
+user = new User();
+
 // Seed data for mock users and lectures
-User.insert((instructor_1) => {
+User.insert(user.id, (instructor_1) => {
 	Lecture.insert("COM SCI 130", instructor_1.id, (newLecture) => {
 		console.log("Inserted new lecture with code " + newLecture.joinCode);
 		console.log('Instructor Id is ' + newLecture.instructor);
